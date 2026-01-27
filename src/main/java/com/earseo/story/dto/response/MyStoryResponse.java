752ec32fdd789ef7728757b024a1900d00e9fd2f
manbron236 <minjuncho@naver.com>
2026-01-27 14:34:18 +0900

    [#8] fix : MyStoryResponse StoryAuthorResponse 연결

--- src/main/java/com/earseo/story/dto/response/MyStoryResponse.java
+++ src/main/java/com/earseo/story/dto/response/MyStoryResponse.java
@@ -11,8 +11,8 @@ public record MyStoryResponse(
         @Schema(description = "이야기 ID", example = "1")
         Long storyId,
 
-        @Schema(description = "작성자 닉네임", example = "카피바라")
-        String nickname,
+        @Schema(description = "이야기 작성자")
+        StoryAuthorResponse storyAuthor,
 
         @Schema(description = "이야기 제목", example = "경복궁 근처 맛집")
         String title,
@@ -47,7 +47,7 @@ public record MyStoryResponse(
     public static MyStoryResponse toDto(Story story, List<String> imageUrls) {
         return new MyStoryResponse(
                 story.getId(),
-                story.getStoryAuthor().getNickname(),
+                StoryAuthorResponse.toDto(story.getStoryAuthor()),
                 story.getStoryTitle().getTitle(),
                 story.getContent(),
                 story.getStoryConcept(),
