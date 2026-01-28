810927da4e89d116d845ee356dd443b0ceb7f3ec
manbron236 <minjuncho@naver.com>
2026-01-28 18:07:04 +0900

    [#8] fix : 주석 삭제

--- src/main/java/com/earseo/story/service/LikeService.java
+++ src/main/java/com/earseo/story/service/LikeService.java
@@ -7,7 +7,6 @@ import com.earseo.story.repository.StoryLikeRepository;
 import com.earseo.story.repository.StoryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,7 +26,6 @@ public class LikeService {
 
     private final StoryRepository storyRepository;
     private final StoryLikeRepository storyLikeRepository;
-//    private final RedisTemplate<String, String> stringTemplate;
     private final StringRedisTemplate stringTemplate;
 
     @Transactional
